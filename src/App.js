07b7c9f5146b3f140a99a6e0bfcc48ed4344b1a4
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2023-05-15 16:33:52 +0530

    given work in progress 15 may 2023

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Home from "./Components/Home/Home";
 export default function App() {
   return (
     <>
-      <span>test from office</span>
-      <br />
-      <span>aditya kumar</span>
+      <Home />
     </>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import "./Home.css";
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <div className="home">
+        <span style={{ fontFamily: "Sans-serif" }}>WORK</span>
+        <div>
+          <span style={{ fontFamily: "Cursive" }}>-- in --</span>
+        </div>
+        <span style={{ fontFamily: "Sans-serif" }}>PROGRESS</span>
+      </div>
     </>
   );
 }
