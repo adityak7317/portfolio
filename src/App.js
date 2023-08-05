8cf7da20fc81843bf6d3e9131176fed7338c4690
ADITYA KUMAR <CodifyWithAditya@Gmail.com>
2023-08-05 19:04:04 +0530

    installed bootstrap..., added navbar..., title name change...........adi--office 5aug23

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const center = {
   width: "100%",
@@ -85,6 +91,41 @@ export default function App() {
           </li>
         </ul>
       </header>
+
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand className="m-0">Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          {/* <div> */}
+          <Navbar.Collapse
+            id="navbarScroll"
+            // className="d-flex justify-content-end"
+          >
+            <Nav className="" navbarScroll>
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>About</Nav.Link>
+              <Nav.Link>Experience</Nav.Link>
+              <Nav.Link>Skills</Nav.Link>
+              <Nav.Link>Works</Nav.Link>
+              <Nav.Link>Contact</Nav.Link>
+              {/* <Nav.Link >Link</Nav.Link> */}
+              {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown> */}
+              {/* <Nav.Link disabled>Link</Nav.Link> */}
+            </Nav>
+          </Navbar.Collapse>
+          {/* </div> */}
+        </Container>
+      </Navbar>
+
       <div ref={section1} style={{ backgroundColor: "gray", ...center }}>
         SECTION 1
       </div>
