3ab5950d8189f25134e4ab4bfbd03a00aaf63560
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-10 21:17:22 +0530

    color pallets and navbar changes ......10aug23

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,5 @@
 import React from "react";
-const element = [];
-{
-  for (let i = 0; i < 100; i++) {
-    element.push("element___" + i);
-  }
-}
+
 export default function Home() {
-  return (
-    <>
-      {element.map((val) => (
-        <div className="col">
-          <span>{val}</span>
-        </div>
-      ))}
-    </>
-  );
+  return <div>Home</div>;
 }
