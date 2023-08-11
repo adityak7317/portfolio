44781d112813502eab089a72d75f59e69fb4426b
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-11 07:34:59 +0530

    styling to photo in home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 import photo from "../../static/photo.png";
+import pic from "../../static/pic.png";
 
 export default function Home() {
   return (
     <>
       <div className="row">
         <div className="col-md-6">some texts</div>
-        <div className="col-md-6">
+        <div
+          className="col-md-6 
+        images"
+        >
+          {/* d-flex justify-content-center p-5  */}
           <img
+            className="pic1"
+            src={pic}
+            alt="pic photo"
+            // style={{
+            //   width: "55%",
+            //   height: "100%",
+            //   // objectFit: "cover"
+            // }}
+          />
+          <img
+            className="pic2"
             src={photo}
             alt="my photo"
-            style={{
-              width: "80%",
-              height: "100%",
-              // objectFit: "cover"
-            }}
+            // style={{
+            //   width: "55%",
+            //   height: "100%",
+            //   // objectFit: "cover"
+            // }}
           />
         </div>
       </div>
