f870f1db9bcf1ee6761c4cc7c04a3e9535c02c3f
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-22 23:15:16 +0530

    something in about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,108 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      {/* <div className="row">
+        <div
+          className="col-md-6"
+          style={{
+            minHeight: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <span>left</span>
+          </div>
+        </div>
+        <div
+          className="col-md-6"
+          style={{
+            // minHeight: "50vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <span>right</span>
+          </div>
+        </div>
+      </div> */}
+      Welcome to my portfolio! I'm Aditya Kumar, a passionate and skilled Full
+      Stack Developer with two years of valuable professional experience. My
+      journey in the world of software development has been driven by a deep
+      fascination for crafting innovative digital solutions that bridge the gap
+      between complexity and simplicity. Throughout my career, I have honed my
+      expertise in a diverse array of technologies, allowing me to provide
+      end-to-end solutions that seamlessly integrate user-centric front-end
+      experiences with robust and scalable back-end functionalities. My toolkit
+      includes Java, JavaScript, and Python, which empower me to create dynamic
+      and responsive applications. I'm well-versed in crafting interactive user
+      interfaces using ReactJS and harnessing the power of Node.js for efficient
+      server-side development. Moreover, my proficiency extends to the realm of
+      database management, as I excel in working with MySQL to ensure data
+      integrity and accessibility. Additionally, I've ventured into the world of
+      containerization with Kubernetes, adding another layer of versatility to
+      my skill set. I am passionate about staying ahead of industry trends and
+      pushing the boundaries of what's possible in the world of development. I
+      invite you to explore the projects showcased here, each reflecting my
+      commitment to innovation, functionality, and elegant design. Thank you for
+      visiting and considering my portfolio.
+    </>
+  );
 }
+
+// <>
+//   <div className="row">
+//     <div className="col-md-6">
+//       <div>
+//         <span>
+//           Welcome to my portfolio! I'm Aditya Kumar, a passionate and
+//           skilled Full Stack Developer with two years of valuable
+//           professional experience. My journey in the world of software
+//           development has been driven by a deep fascination for crafting
+//           innovative digital solutions that bridge the gap between
+//           complexity and simplicity. Throughout my career, I have honed my
+//           expertise in a diverse array of technologies, allowing me to
+//           provide end-to-end solutions that seamlessly integrate
+//           user-centric front-end experiences with robust and scalable
+//           back-end functionalities. My toolkit includes Java, JavaScript,
+//           and Python, which empower me to create dynamic and responsive
+//           applications. I'm well-versed in crafting interactive user
+//           interfaces using ReactJS and harnessing the power of Node.js for
+//           efficient server-side development. Moreover, my proficiency
+//           extends to the realm of database management, as I excel in working
+//           with MySQL to ensure data integrity and accessibility.
+//           Additionally, I've ventured into the world of containerization
+//           with Kubernetes, adding another layer of versatility to my skill
+//           set. I am passionate about staying ahead of industry trends and
+//           pushing the boundaries of what's possible in the world of
+//           development. I invite you to explore the projects showcased here,
+//           each reflecting my commitment to innovation, functionality, and
+//           elegant design. Thank you for visiting and considering my
+//           portfolio.
+//         </span>
+//       </div>
+//     </div>
+//     <div className="col-md-6">
+//       <span>right side</span>
+//     </div>
+//   </div>
+// </>
