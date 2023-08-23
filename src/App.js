0c68ea6cf716410a3cc4db5e9d56849e194ab702
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-08-24 00:33:01 +0530

    added the navbar and other stuffs to new ...adi 24aug23

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
 import React from "react";
+import "./App.css";
+
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
+  const style = {
+    components: {
+      backgroundColor: "#DDE6ED",
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  };
   return (
     <>
+      <ToastContainer />
+
       <Navbar />
-      <Home />
-      <About />
+
+      <div className="components" style={{ ...style.components }}>
+        <div style={{ ...style.components, backgroundColor: "#DDE6ED" }}>
+          <div className="SpaceBWNavAndCom"></div>
+          <div
+            className="d-flex flex-column justify-content-center align-items-center"
+            // style={{}}
+          >
+            <span>
+              Everything is simple, but for this you need to master!!!
+            </span>
+            <span className="p-1"></span>
+          </div>
+        </div>
+        <div style={{ ...style.components, backgroundColor: "#9DB2BF" }}>
+          <Home />
+        </div>
+        <div style={{ ...style.components, backgroundColor: "#526D82" }}>
+          <About />
+        </div>
+        <div style={{ ...style.components, backgroundColor: "#27374D" }}>
+          <Experience />
+        </div>
+        <div style={{ ...style.components, backgroundColor: "#526D82" }}>
+          <Projects />
+        </div>
+        <div style={{ ...style.components, backgroundColor: "#9DB2BF" }}>
+          <Contact />
+        </div>
+        <div className="bg-dark text-white" style={{ ...style.components }}>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>
+        home data Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatem neque nisi rem eum doloremque ut omnis incidunt iste quidem
+        sit, repudiandae porro consectetur temporibus delectus quis libero unde.
+        Nesciunt, temporibus.
+      </div>
+    </>
+  );
 }
