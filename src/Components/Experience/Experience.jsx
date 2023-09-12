512b2f0a82773bea0f0b157f04c39f06bfcffcb2
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-09-12 21:10:23 +0530

    work in experience section....adi 12sep23

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,15 +1,52 @@
 import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function ControlledAccordions() {
-  const [expanded, setExpanded] = React.useState(false);
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
   };
 
   return (
@@ -18,44 +55,42 @@ export default function ControlledAccordions() {
         expanded={expanded === "panel1"}
         onChange={handleChange("panel1")}
       >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1bh-content"
-          id="panel1bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            General settings
-          </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            I am an accordion
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography style={{ width: "100%" }}>
+            <div className="row">
+              <div className="col-md-9">
+                <span>software Engineer @ ahana systems and solutions</span>
+              </div>
+              <div className="col-md-3">
+                <div className="d-flex justify-content-end">
+                  <span>dec,2021 -present</span>
+                </div>
+              </div>
+            </div>
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-            Aliquam eget maximus est, id dignissim quam.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion
+      {/* <Accordion
         expanded={expanded === "panel2"}
         onChange={handleChange("panel2")}
       >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>Users</Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            You are currently not an owner
-          </Typography>
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>Collapsible Group Item #2</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat
-            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-            laoreet laoreet.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -63,45 +98,18 @@ export default function ControlledAccordions() {
         expanded={expanded === "panel3"}
         onChange={handleChange("panel3")}
       >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
         </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel4"}
-        onChange={handleChange("panel4")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Personal data
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+      </Accordion> */}
     </div>
   );
 }
@@ -128,56 +136,25 @@ export default function ControlledAccordions() {
 //       "NodeJS",
 //     ],
 //   },
+//   {
+//     companyName: "Ahana Systems and Solutions",
+//     companyLink: "https://ahanait.com/",
+//     position: "Software Engineer",
+//     duration: "Dec, 2021 - Present",
+//     location: "Bangalore, IN",
+//     logoImg: "https://ahanait.com/wp-content/uploads/2023/01/logo-new-1.svg",
+//     logoAlt: "Ahana Logo",
+//     description:
+//       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel reprehenderit ex id, adipisci nobis aperiam, voluptas quos nam distinctio voluptatem voluptatum consequatur facere ipsum sed molestias numquam dolorem nulla.",
+//     skills: [
+//       "ReactJS",
+//       "NodeJS",
+//       "ReactJS",
+//       "NodeJS",
+//       "ReactJS",
+//       "NodeJS",
+//       "ReactJS",
+//       "NodeJS",
+//     ],
+//   },
 // ];
-
-// <div>
-//         {experienceContent.map((val, i) => (
-//           <Accordion
-//             expanded={expanded === "panel1"}
-//             onChange={handleChange("panel1")}
-//           >
-//             <AccordionSummary
-//               aria-controls="panel1d-content"
-//               id="panel1d-header"
-//             >
-//               <Typography>
-//                 <div>
-//                   <span>{val.position}</span>
-//                   <span> @</span>
-//                   <span>{val.companyName}</span>
-//                 </div>
-
-//                 <div>
-//                   <span>{val.duration}</span>
-//                 </div>
-//               </Typography>
-//             </AccordionSummary>
-//             <AccordionDetails>
-//               <Typography>
-//                 <div className="row">
-//                   <div className="col-md-1">
-//                     {/* <img
-//                       src={val.logoImg}
-//                       alt={val.logoAlt}
-//                       // style={{ width: "max-content" }}
-//                     /> */}
-//                   </div>
-//                   <div className="col-md-11">
-//                     <div className="row">
-//                       <span>{val.location}</span>
-
-//                       <span>{val.description}</span>
-
-//                       <div className="col">
-//                         {val.skills.map((valSkills, j) => (
-//                           <span>{valSkills}</span>
-//                         ))}
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </Typography>
-//             </AccordionDetails>
-//           </Accordion>
-//         ))}
-//       </div>
