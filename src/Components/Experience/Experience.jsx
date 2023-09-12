e2e00b9a6c0d487af4ca5ec54789f22863e5400b
Aditya Kumar <CodifyWithAditya@Gmail.com>
2023-09-12 23:42:43 +0530

    experience page work...adi 12sep23

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -6,6 +6,11 @@ import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+
+import { MdLocationPin } from "react-icons/md";
+
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
@@ -42,6 +47,53 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
+const experienceContent = [
+  {
+    companyName: "Ahana Systems and Solutions",
+    companyLink: "https://ahanait.com/",
+    position: "Software Engineer",
+    start: "Dec, 2021",
+    end: "Present",
+    location: "Bangalore, IN",
+    logoImg: "https://ahanait.com/wp-content/uploads/2023/01/logo-new-1.svg",
+    logoAlt: "Ahana Logo",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel reprehenderit ex id, adipisci nobis aperiam, voluptas quos nam distinctio voluptatem voluptatum consequatur facere ipsum sed molestias numquam dolorem nulla.",
+    skills: [
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+    ],
+  },
+  {
+    companyName: "Ahana Systems and Solutions",
+    companyLink: "https://ahanait.com/",
+    position: "Software Engineer",
+    start: "Dec, 2021",
+    end: "Present",
+    location: "Bangalore, IN",
+    logoImg: "https://ahanait.com/wp-content/uploads/2023/01/logo-new-1.svg",
+    logoAlt: "Ahana Logo",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat vel reprehenderit ex id, adipisci nobis aperiam, voluptas quos nam distinctio voluptatem voluptatum consequatur facere ipsum sed molestias numquam dolorem nulla.",
+    skills: [
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+      "ReactJS",
+      "NodeJS",
+    ],
+  },
+];
+
 export default function CustomizedAccordions() {
   const [expanded, setExpanded] = React.useState("panel1");
 
@@ -49,68 +101,108 @@ export default function CustomizedAccordions() {
     setExpanded(newExpanded ? panel : false);
   };
 
+  const handleClick = () => {};
+
   return (
-    <div>
-      <Accordion
-        expanded={expanded === "panel1"}
-        onChange={handleChange("panel1")}
-      >
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography style={{ width: "100%" }}>
-            <div className="row">
-              <div className="col-md-9">
-                <span>software Engineer @ ahana systems and solutions</span>
-              </div>
-              <div className="col-md-3">
-                <div className="d-flex justify-content-end">
-                  <span>dec,2021 -present</span>
+    <>
+      <h2 className="text-white">Professional Experience</h2>
+      <div className="p-3"></div>
+      <div>
+        {experienceContent.map((expVal, i) => (
+          <Accordion
+            expanded={expanded === `panel${i + 1}`}
+            onChange={handleChange(`panel${i + 1}`)}
+            className="mb-2"
+            style={{ borderRadius: "5px" }}
+            // style={{
+            //   background:
+            //     "linear-gradient(120deg, rgb(166 181 189) 32%, rgb(108 112 115) 100%)",
+            // }}
+          >
+            <AccordionSummary
+              aria-controls="panel1d-content"
+              id="panel1d-header"
+            >
+              <Typography style={{ width: "100%" }}>
+                <div className="row">
+                  <div className="col-md-9">
+                    <span>
+                      {expVal.position} @ {expVal.companyName}
+                    </span>
+                  </div>
+                  <div className="col-md-3">
+                    <div className="d-flex justify-content-end">
+                      <span>
+                        {expVal.start} - {expVal.end}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {/* <Typography style={{ width: "100%" }}>
+              <div className="row">
+                <div className="col-md-1">
+                  <img src={expVal.logoImg} alt={expVal.logoAlt} />
+                </div>
+                <div className="col-md-11">
+                  <div className="row">
+                    <span>{expVal.location}</span>
+                    <span>{expVal.description}</span>
+                    <span>
+                      {expVal.skills.map((skillVal, j) => (
+                        <span>{skillVal}</span>
+                      ))}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      {/* <Accordion
-        expanded={expanded === "panel2"}
-        onChange={handleChange("panel2")}
-      >
-        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel3"}
-        onChange={handleChange("panel3")}
-      >
-        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
-    </div>
+            </Typography> */}
+              <Typography style={{ width: "100%" }}>
+                <div className="row">
+                  <div className="col-md-1 d-flex">
+                    <img src={expVal.logoImg} alt={expVal.logoAlt} />
+                  </div>
+                  <div className="col-md-11">
+                    <div className="row">
+                      <span>
+                        <span>
+                          <MdLocationPin />
+                        </span>
+                        <span>{expVal.location}</span>
+                      </span>
+                      <span>{expVal.description}</span>
+                      {/* <Stack direction="row" spacing={1}> */}
+                      <span>
+                        {expVal.skills.map((skillVal, j) => (
+                          <span>
+                            <Chip
+                              label={skillVal}
+                              variant="outlined"
+                              onClick={handleClick}
+                            />
+                            {"    "}
+                          </span>
+                        ))}
+                      </span>
+                      {/* </Stack> */}
+                      {/* <span>
+                        <span>{skillVal} , </span>
+                    </span> */}
+                    </div>
+                  </div>
+                </div>
+                {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus
+              quidem, facere tempore qui dolor repudiandae et veritatis
+              quibusdam nostrum facilis beatae temporibus inventore ea
+              architecto. Vel omnis deleniti architecto delectus? */}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </>
   );
 }
 
